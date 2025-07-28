dcb0715b24106b773db9c270fbaec71e4f44a6c4
Uplupilu <honglionay@gmail.com>
2025-07-28 22:16:04 +0300

    Create colors.js

---
+++ colors.js
@@ -0,0 +1,27 @@
+// Массив нитей: бренды, коды, описания и HEX‑цвета
+const threads = [
+  {
+    brand: "PROWRAP METALLIC",
+    code: "9850",
+    description: "SIZE A MS-A 100yds",
+    hex: "#b08d57"
+  },
+  {
+    brand: "PROWRAP Color Fast",
+    code: "122",
+    description: "Size D CFS-D 100yds",
+    hex: "#a0522d"
+  },
+  {
+    brand: "Fuji Ultra Poly™",
+    code: "003",
+    description: "Size D (D-Ultra) Color",
+    hex: "#3d5c91"
+  },
+  {
+    brand: "Fuji Ultra Poly™ Metallic",
+    code: "910",
+    description: "Aqua (металлическая)",
+    hex: "#00cccc"
+  }
+];
