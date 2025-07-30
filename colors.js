27b467992b9240ee8c2f470c7551168200991ff7
Uplupilu <honglionay@gmail.com>
2025-07-30 22:40:32 +0300

    Update colors.js more colors

--- colors.js
+++ colors.js
@@ -24,7 +24,285 @@ const threads = [
     code: "910",
     description: "Aqua (металлическая)",
     hex: "#00cccc"
+  },
+    // Fuji NOCP Thread – Size A (100 M)
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "BLACK-001",
+    description: "100M",
+    hex: "#000000"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "WHITE-002",
+    description: "100M",
+    hex: "#FFFFFF"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "DK GRN-003",
+    description: "100M",
+    hex: "#006400"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "LT GRN-004",
+    description: "100M",
+    hex: "#90EE90"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "SCARLET-005",
+    description: "100M",
+    hex: "#FF2400"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "MAROON-006",
+    description: "100M",
+    hex: "#800000"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "GARNET-007",
+    description: "100M",
+    hex: "#733635"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "DK BLUE-008",
+    description: "100M",
+    hex: "#00008B"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "RYL BLUE-009",
+    description: "100M",
+    hex: "#4169E1"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "BL DUNN-010",
+    description: "100M",
+    hex: "#5B92E5"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "CHSNT-011",
+    description: "100M",
+    hex: "#954535"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "DK BRN-012",
+    description: "100M",
+    hex: "#654321"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "YLW-013",
+    description: "100M",
+    hex: "#FFFF00"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "GLDNROD-014",
+    description: "100M",
+    hex: "#DAA520"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "ORNG-015",
+    description: "100M",
+    hex: "#FFA500"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "PURP-016",
+    description: "100M",
+    hex: "#800080"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "TEAL-017",
+    description: "100M",
+    hex: "#008080"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "NAVY-018",
+    description: "100M",
+    hex: "#000080"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "LT GREY-019",
+    description: "100M",
+    hex: "#D3D3D3"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "CAN AP-020",
+    description: "100M",
+    hex: "#D2232A"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "LT BLUE-021",
+    description: "100M",
+    hex: "#ADD8E6"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "HOT PINK-022",
+    description: "100M",
+    hex: "#FF69B4"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "MAUI WOWIE-024",
+    description: "100M",
+    hex: "#00BFFF"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "BRACKET BLUE-025",
+    description: "100M",
+    hex: "#4F81BD"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "MOSS BROWN-026",
+    description: "100M",
+    hex: "#8B5A2B"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "MOSS GREEN-027",
+    description: "100M",
+    hex: "#8A9A5B"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "OLIVE GREEN-028",
+    description: "100M",
+    hex: "#808000"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "DESERT TAN-029",
+    description: "100M",
+    hex: "#D2B48C"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "SECRET SQUIRREL-030",
+    description: "100M",
+    hex: "#8B8680"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "MIDNIGHT-031",
+    description: "100M",
+    hex: "#191970"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "TACTICAL TAN-032",
+    description: "100M",
+    hex: "#BD9B60"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "FLAT DARK EARTH-033",
+    description: "100M",
+    hex: "#6A5D4D"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "OG GREEN-034",
+    description: "100M",
+    hex: "#228B22"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "AQUA MARINE-035",
+    description: "100M",
+    hex: "#7FFFD4"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "CONCRETE-036",
+    description: "100M",
+    hex: "#95A5A6"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "RAIN CLOUD-037",
+    description: "100M",
+    hex: "#778899"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "PURPLE-038",
+    description: "100M",
+    hex: "#8A2BE2"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "COBALT BLUE-039",
+    description: "100M",
+    hex: "#0047AB"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "BUBBLEGUM PINK-040",
+    description: "100M",
+    hex: "#FFC1CC"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "CORK-041",
+    description: "100M",
+    hex: "#C49E7C"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "TAXI CAB YELLOW-042",
+    description: "100M",
+    hex: "#FFD700"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "CHEDDAR-043",
+    description: "100M",
+    hex: "#FFB300"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "SPRING GREEN-044",
+    description: "100M",
+    hex: "#00FF7F"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "NEON ORNG-501",
+    description: "100M",
+    hex: "#FF5F1F"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "NEON PINK-502",
+    description: "100M",
+    hex: "#FF6EC7"
+  },
+  {
+    brand: "Fuji NOCP Thread - Size A",
+    code: "NEON GRN-503",
+    description: "100M",
+    hex: "#39FF14"
   }
+
 ];
 
 // экспортируем в глобальную переменную
